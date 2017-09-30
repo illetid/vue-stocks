0497c3af7568e66eba65a14243d721b165669090
aovcharenko <andrey.ocharenko@beetroot.se>
2017-09-30 15:48:54 +0300

    added dist to repo

---
+++ dist/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname));
+app.get('*', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
